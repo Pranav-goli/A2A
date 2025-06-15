9e1ec4f180388b204934413ecb01c862e6f282f6
Pranav-goli <101007720+Pranav-goli@users.noreply.github.com>
2025-06-15 18:29:38 +0530

    Update agent.py

--- app/agent.py
+++ app/agent.py
@@ -3,14 +3,12 @@ from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParamet
 from dotenv import load_dotenv
 load_dotenv()
 
-# ABSOLUTE_FILE_PATH = "C:\\Users\\Asus\\Downloads\\test
 ABSOLUTE_FILE_PATH = "C:\\Users\\Pranav\\OneDrive\\Desktop\\Assignments"
-
 
 root_agent = LlmAgent(
     model='gemini-2.0-flash',
     name='filesystem_assistant_agent',
-    instruction='Help the user manage their files. You can list files, read files, etc. You can also read files stored in Google Drive.',
+    instruction='Help the user to manage their files. You can list files, read files, etc.',
     tools=[
         MCPToolset(
             connection_params=StdioServerParameters(
@@ -21,8 +19,6 @@ root_agent = LlmAgent(
                     ABSOLUTE_FILE_PATH
                 ]
             ),
-            # Optional: Filter which tools from the MCP server are exposed
-            # tool_filter=['list_directory', 'read_file']
         )
     ],
 )
